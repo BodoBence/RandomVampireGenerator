29655dd5c73a6b9efa63cfd9d61d81902fa75106
BodoBence <cyke216@gmail.com>
2021-04-26 15:00:38 +0200

    a server sandbox good, de le kell kezelni még a rövidebb nested dict-eket

--- sandbox.py
+++ sandbox.py
@@ -1,26 +1,21 @@
-test_dictionary = {'level11': {'level21': 'a',
-                               'level22': 'b'},
-                    'level12': {'level23': 'x',
-                                'level24': 'y'}}
-
-print(test_dictionary)
+from default_data import sample_character_sheet
 
 
+sample_data = sample_character_sheet()
 
-def stat_block_for_flask_table(number_of_stat_key_value_pairs,
-                                dictionary_container):
+def testing(dictionary_container, number_of_stat_key_value_pairs):
 
     stat_lists_container = {}
+    i = 1
     for level1 in dictionary_container.keys():
-        for i in range(1, number_of_stat_key_value_pairs + 1):
-            stat_list_value_name = "keys" + str(i)
-            stat_list_key_name = "values" + str(i)
-            stat_list_keys = list(dictionary_container[level1].keys())
-            stat_list_values = list(dictionary_container[level1].values())
-            stat_lists_container[stat_list_value_name] = stat_list_keys
-            stat_lists_container[stat_list_key_name] = stat_list_values
+        stat_list_value_name = "keys" + str(i)
+        stat_list_key_name = "values" + str(i)
+        stat_list_keys = list(dictionary_container[level1].keys())
+        stat_list_values = list(dictionary_container[level1].values())
+        stat_lists_container[stat_list_value_name] = stat_list_keys
+        stat_lists_container[stat_list_key_name] = stat_list_values
+        print(stat_lists_container)
+        i = i+1
     return stat_lists_container
-test_case = stat_block_for_flask_table(2, test_dictionary)
-print(test_case)
 
-
+testing(sample_data['Character_Details'], 2)
--- server_sandbox.py
+++ server_sandbox.py
@@ -18,6 +18,60 @@ class ItemTable(Table):
     col5 = Col('col5')
     col6 = Col('col6')
 
+def stat_block_for_flask_table(number_of_stat_key_value_pairs,
+                               dictionary_container,
+                               current_table,
+                               number_of_cols):
+    # dictionary with n number of  lists for values and keys, names as keys1 = [...], values1 =[...]
+    stat_lists_container = {}
+    
+    level_counter = 1
+    for level1 in dictionary_container.keys():
+
+        stat_list_value_name = "keys" + str(level_counter)
+        stat_list_key_name = "values" + str(level_counter)
+
+        stat_list_keys = list(dictionary_container[level1].keys())
+        stat_list_values = list(dictionary_container[level1].values())
+
+        stat_lists_container[stat_list_value_name] = stat_list_keys
+        stat_lists_container[stat_list_key_name] = stat_list_values
+        
+        level_counter = level_counter +1
+
+    # get largest dictionary + its length
+    longest_sub_dictinary = {}
+    for sub_dictionary_key in dictionary_container.keys():
+        if len(dictionary_container[sub_dictionary_key]) > len(longest_sub_dictinary):
+            longest_sub_dictinary = dictionary_container[sub_dictionary_key]
+    number_of_rows = len(longest_sub_dictinary)
+
+    print('stat_lists_container', stat_lists_container)
+    print('number_of_rows', number_of_rows)
+    print('number_of_stat_key_value_pairs', number_of_stat_key_value_pairs)
+
+    for current_row in range(1, number_of_rows + 1):
+        row_stat_block = {}
+        current_value_pair = 1
+        
+        for current_col in range(1, (number_of_stat_key_value_pairs * 2) + 1):
+
+            if current_col % 2 != 0:
+                print('current_col',current_col)
+                row_stat_block['col' + str(current_col)] = stat_lists_container['keys' + str(current_value_pair)][current_row-1]
+                row_stat_block['col' + str(current_col + 1)] = stat_lists_container['values' + str(current_value_pair)][current_row-1]
+                current_value_pair = current_value_pair + 1
+                print('row_stat_block',row_stat_block)
+        while len(row_stat_block) < number_of_cols:
+            row_stat_block['col' + str(len(row_stat_block) + 1)] = ''
+            
+        print('row_stat_block', row_stat_block)
+
+        current_table.append(row_stat_block)
+    
+
+
+
 def dictionary_to_flask_table(character_dictionary):
     table = []
     # category separator rows
@@ -37,27 +91,10 @@ def dictionary_to_flask_table(character_dictionary):
                                    'col6': ""}
     table.append(row_character_details_types)
     
-    basic_information_stat_names = []
-    basic_information_stat_values = []
-    trackers_stat_names = []
-    trackers_stat_values = []
-
-    for key, value in character_dictionary['Character_Details']['Basic_Information'].items():
-        basic_information_stat_names.append(str(key))
-        basic_information_stat_values.append(str(value))
-
-    for key, value in character_dictionary['Character_Details']['Trackers'].items():
-        trackers_stat_names.append(str(key))
-        trackers_stat_values.append(str(value))
-
-    for x in range(0, (max(len(character_dictionary['Character_Details']['Basic_Information']), len(character_dictionary['Character_Details']['Trackers'])) - 1)):
-        row_stat_block = {'col1': basic_information_stat_names[x],
-                          'col2': basic_information_stat_values[x],
-                          'col3': trackers_stat_names[x] if x < len(trackers_stat_names) else '',
-                          'col4': trackers_stat_values[x] if x < len(trackers_stat_values) else '',
-                          'col5': "",
-                          'col6': ""}
-        table.append(row_stat_block)
+    stat_block_for_flask_table(number_of_stat_key_value_pairs=2,
+                               dictionary_container=character_dictionary['Character_Details'],
+                               current_table=table,
+                               number_of_cols=6)
 
 
     row_attributes ={'col1': 'Attributes',
