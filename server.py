d292702e76e093aaba1bd8252a4f81f9ab67f381
BodoBence <cyke216@gmail.com>
2021-04-30 18:16:54 +0200

    shorter, older vamprie name, + download button (not functinoing yet)

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_table import Table, Col
 from random_vampire_genreator_web import generate
 import default_data  
