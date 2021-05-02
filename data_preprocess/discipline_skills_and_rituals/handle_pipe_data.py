2ac729c4fd71b2cba4148197a25edb0cd9db65d0
BodoBence <cyke216@gmail.com>
2021-05-02 16:52:11 +0200

    have  description skill and ritual csv with levels and seperate skill names and descriptions

--- data_preprocess/discipline_skills_and_rituals/handle_pipe_data.py
+++ data_preprocess/discipline_skills_and_rituals/handle_pipe_data.py
@@ -26,7 +26,20 @@ with open(pipe_shit_filename) as tsvfile:
 with open(output_shit_filename, "w") as outfile:
     for i in range(1, counter):
         row_name = f'row{i}'
-        row = '\t'.join(storage[row_name])
+        row_elements = storage[row_name]
+        if i == 1:
+            row_elements.append('Description')
+        else: 
+            try:
+                name, description = row_elements[-1].split(':', 1)
+            except ValueError:
+                name = row_elements[-1]
+                description = 'N/A'
+
+            row_elements[-1] = name
+            row_elements.append(description)
+
+        row = '\t'.join(row_elements)
         outfile.write(f'{row}\n')
 
 # pprint.pprint(storage)
