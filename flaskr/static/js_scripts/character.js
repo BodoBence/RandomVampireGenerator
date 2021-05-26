cde1e91bcdbe8c80e4a8b4b72b8ab6e368a9eacf
BodoBence <cyke216@gmail.com>
2021-05-26 02:43:47 +0200

    working on the animation of discipline skills

--- flaskr/static/js_scripts/character.js
+++ flaskr/static/js_scripts/character.js
@@ -1,4 +1,5 @@
-create_event_listener_for_skills("button_discipline_skills", "discipline_skills");
+// create_event_listener_for_skills("button_discipline_skills", "discipline_skills");
+create_global_event_listener("click", "button_discipline_skills", toggle_discipline_skills, false)
 
 create_global_event_listener("click", "print_vampire", print_character, true)
 
@@ -18,6 +19,19 @@ function create_event_listener_for_skills(class_name, target_class_name){
         })
     }
 }
+
+function toggle_discipline_skills (pressed_button){
+    animating_class = pressed_button.getAttribute("data-toggle-animation-class")
+    selection_class = pressed_button.getAttribute("data-toggle-selection-class")
+    target_reference = pressed_button.getAttribute("data-toggle-reference")
+
+    target_element = document.getElementsByClassName(selection_class)[target_reference]
+    target_element_id = target_element.getAttribute("id")
+
+    toggle_class_based_animation(target_element_id, animating_class, true)
+
+}
+
 
 function print_character(){
     console.log("hi")
