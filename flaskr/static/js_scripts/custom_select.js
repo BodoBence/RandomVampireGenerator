7db9e21b32b08d1e6b4a8df88d2948753d33de92
BodoBence <cyke216@gmail.com>
2021-06-07 21:07:49 +0200

    working on the custom arrow

--- flaskr/static/js_scripts/custom_select.js
+++ flaskr/static/js_scripts/custom_select.js
@@ -4,38 +4,45 @@ create_global_event_listener('click', 'dropdown_button', handle_custom_select, '
 create_global_event_listener('click', 'fake_option', update_select_with_custom, 'class')
 
 function handle_custom_select(activated_custom_select){
+  console.log("Handling custom dropdown")
+
   // Reveal/Hide the custom select options
-  custom_options_containter_id = activated_custom_select.getAttribute('data-reference-id')
-  custom_options_container = document.getElementById(custom_options_containter_id)
+  let custom_options_containter_id = activated_custom_select.getAttribute('data-reference-id')
+  let custom_options_container = document.getElementById(custom_options_containter_id)
+
   custom_options_container.classList.toggle("show");
 
-  current_custom_arrow = activated_custom_select.parentElement.children.getElementsByClass("custom_arrow")
-  console.log(current_custom_arrow)
-  animation_rotate_element(current_custom_arrow[0])
+  // Trigeer cusotm arrow animaiton
+  toggle_arrow_animation(activated_custom_select)
 
+  // Add close listener
   add_close_listener(custom_options_container, activated_custom_select)
 }
 
 function update_select_with_custom(my_trigger) {
-    console.log('updating the hidden select field with the fake selects manual input')
-    target_select_id = my_trigger.getAttribute('data-reference-id')
-    target_select = document.getElementById(target_select_id)
-    target_option_value = my_trigger.getAttribute('data-reference-option')
-    
-    target_select.value = target_option_value
-    console.log("the new selcetion value is:")
-    console.log(target_select.value)
+  console.log('updating the hidden select field with the fake selects manual input')
 
-    // Set fake dropdown select elemet's text to match the chosen selection
-    set_dropdown_text(my_trigger)
-    // Close the dropdown
-    my_trigger.parentElement.classList.toggle("show")
+  let target_select_id = my_trigger.getAttribute('data-reference-id')
+  let target_select = document.getElementById(target_select_id)
+  let target_option_value = my_trigger.getAttribute('data-reference-option')
+
+  target_select.value = target_option_value
+
+  // Set fake dropdown select elemet's text to match the chosen selection
+  set_dropdown_text(my_trigger)
+
+  // Close the dropdown
+  my_trigger.parentElement.classList.toggle("show")
+
+  // Trigeer cusotm arrow animaiton
+  toggle_arrow_animation(my_trigger)
+
 }
 
 function set_dropdown_text(chosen_option){
-  new_value = chosen_option.innerHTML
-  target_id = chosen_option.getAttribute('data-reference-id-button')
-  target = document.getElementById(target_id).firstElementChild
+  let new_value = chosen_option.innerHTML
+  let target_id = chosen_option.getAttribute('data-reference-id-button')
+  let target = document.getElementById(target_id).firstElementChild
   console.log(target)
 
   target.innerHTML = new_value
@@ -44,22 +51,21 @@ function set_dropdown_text(chosen_option){
 function add_close_listener(open_dropdown, button){
   window.addEventListener('click', e => {
     if (e.target != button) {
+      // Clsoe the dropdown menu
       open_dropdown.classList.remove("show")
+      
+      // Trigeer cusotm arrow animaiton
+      toggle_arrow_animation(button)
     }
   })
 }
 
-function animation_rotate_element(current_trigger){
-  console.log(current_trigger)
-  animation_target = current_trigger.nextElementSibling
-  console.log(animation_target)
-  
-  if (animation_target.getAttribute('class') == 'custom_arrow') {
+function toggle_arrow_animation(current_trigger){
+  // console.log('getting animation target')
+  let current_target_id = current_trigger.getAttribute("data-reference-id-arrow")
+  let current_target = document.getElementById(current_target_id)
 
-      if (animation_target.style.transform == "rotate(225deg)") {
-          animation_target.style.transform = "rotate(45deg)"
-      } else {
-          animation_target.style.transform = "rotate(225deg)"
-      }
-  }
+  current_target.classList.toggle("animation_rotate")
+  // console.log('the animation target is')
+  // console.log(current_target)
 }
