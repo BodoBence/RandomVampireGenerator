fc07027b4e5074240e4e1f9f12b6fdd1baec3400
BodoBence <cyke216@gmail.com>
2021-06-11 12:25:49 +0200

    cleanup

--- flaskr/main.py
+++ flaskr/main.py
@@ -102,10 +102,6 @@ def calculation_maths():
 def encounter_tracker():
     return render_template('encounter_tracker.html')
 
-@app.route('/test',  methods = ['POST', 'GET'])
-def test():
-    return render_template('test.html')
-
 @app.route('/collection_start', methods = ['POST', 'GET'])
 def collection_start():
     with open(CHARACTER_INDEX) as index:
