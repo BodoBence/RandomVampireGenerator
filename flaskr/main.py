802fd766390036f03bea46674b5ff42fc1e4f223
BodoBence <cyke216@gmail.com>
2021-07-19 12:17:44 +0200

    Version 1.05 as deployed on http://autofeed.pythonanywhere.com/ 604f48c

--- flaskr/main.py
+++ flaskr/main.py
@@ -14,7 +14,7 @@ app.secret_key = unique_key = str(uuid.uuid1())
 # Startup variables
 
 # Metadata
-VERSION_INFO = '1.04'
+VERSION_INFO = '1.05'
 
 # Generator defaults
 startup_input_field_details = {
