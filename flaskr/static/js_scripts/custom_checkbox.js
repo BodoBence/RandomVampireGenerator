dd4b5f4224cd9ed827a0ea1e8a68727e07777aa4
BodoBence <cyke216@gmail.com>
2021-08-01 17:08:48 +0200

    update

---
+++ flaskr/static/js_scripts/custom_checkbox.js
@@ -0,0 +1,14 @@
+create_global_event_listener('click', 'custom_checkbox', update_input_checkbox, 'class')
+
+function update_input_checkbox(current_trigger){
+    input_checkbox_id = current_trigger.getAttribute('data-reference-id')
+    input_checkbox_element = document.getElementById(input_checkbox_id)
+    
+    current_trigger.classList.toggle("custom_checkbox_clicked")
+
+    if (input_checkbox_element.checked == true) {
+        input_checkbox_element.checked = false
+    } else {
+        input_checkbox_element.checked = true
+    }
+}
