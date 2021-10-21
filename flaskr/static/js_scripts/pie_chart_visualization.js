3e0e8df56141453aee1f5600b31ffcd7288d0145
BodoBence <cyke216@gmail.com>
2021-10-22 00:30:57 +0200

    Main. BEtter segmetn drawring

--- flaskr/static/js_scripts/pie_chart_visualization.js
+++ flaskr/static/js_scripts/pie_chart_visualization.js
@@ -1,61 +1,54 @@
-initialize_pie_chart('circle_pie_sector', 'pie_input')
+create_global_event_listener('change', 'pie_input', update_pie, 'class')
 
-function initialize_pie_chart(class_name_sectors, class_name_inputs) {
-    var pie_chart_sectors = document.getElementsByClassName(class_name_sectors)
-    var pie_chart_inputs = document.getElementsByClassName(class_name_inputs)
-    var pie_svg = pie_chart_sectors[0].parentElement
-    // Create as many pie sector as there are inputs
-    // Guard check
-    if ((pie_chart_inputs.length - pie_chart_sectors.length) <= 0){
-        console.log('Too many circle_pie_sector elements in the doc')
-    } else {
-        while ((pie_chart_inputs.length - pie_chart_sectors.length) > 0) {
-            let clone = pie_chart_sectors[pie_chart_sectors.length - 1].cloneNode(true)
-            pie_svg.appendChild(clone)
-        }
-    }
-    // set circle sector values
-    console.log('---------')
-    console.log(pie_chart_sectors)
-    console.log('---------')
+function update_pie() {
+    let pie_input_elements = document.getElementsByClassName('pie_input')
+    let sum_of_inputs = get_sum_of_inputs(pie_input_elements)
+    let list_of_percentages = []
 
-    // check if the nubmers match
-    if (pie_chart_inputs.length == pie_chart_sectors.length){
-        let sector_degrees = []
-        for (let index = 0; index < pie_chart_inputs.length; index++) {
-            let perimeter = pie_chart_sectors[index].getAttribute("r") * 2 * 3.14
-            let percentage = calculate_pie_input_sum(pie_chart_inputs) / pie_chart_inputs[index].value
-            let degree = 360 * percentage
-            sector_degrees.push(degree)
-            let accumulated_degrees = sum_sector_degrees(sector_degrees, index)
-            pie_chart_sectors[index].style.strokeDasharray =  perimeter
-            pie_chart_sectors[index].setAttribute('data-pie_percentage', percentage)
-            pie_chart_sectors[index].style.strokeDashoffset =  perimeter * (1 - percentage)
-            pie_chart_sectors[index].style.zIndex = index
-            pie_chart_sectors[index].style.transform = "rotateZ(" + accumulated_degrees + "deg)"
-        }
-    }
+    for (let index = 0; index < pie_input_elements.length; index++) {
+        let target_percentage = pie_input_elements[index].value / sum_of_inputs
+        list_of_percentages.push(target_percentage * 360)
+        let pie_segment_id = pie_input_elements[index].getAttribute('data-reference-pie-id')
+        let pie_segment = document.getElementById(pie_segment_id)
+        set_pie_sector_length(pie_segment, target_percentage)   
+        set_pie_sector_rotation(pie_segment, index, list_of_percentages)
 
-    function sum_sector_degrees(sector_degrees, input_index) {
-        sum = 0
-        if (input_index == 0) {
-            return sum
-        } else {
-            for (let index = 0; index < input_index; index++) {
-                sum = sum + sector_degrees[index] 
-            }
-            return sum
-        }    
-    }
-
-    function calculate_pie_input_sum(pie_inputs) {
-        sum = 0
-        for (let index = 0; index < pie_inputs.length; index++) {
-            sum = sum + pie_inputs[index].value
-        }
-        return sum
     }
 }
 
+function get_sum_of_inputs(pie_input_elements) {
+    let sum_of_inputs = 0
+    for (let index = 0; index < pie_input_elements.length; index++) {
+        sum_of_inputs = sum_of_inputs + parseInt(pie_input_elements[index].value)
+    }
+    return sum_of_inputs
+}
 
+function set_pie_sector_length(pie_segmnet, target_percentage) {
+    let perimeter = pie_segmnet.getAttribute("r") * 2 * Math.PI
+    pie_segmnet.style.strokeDasharray =  perimeter
+    pie_segmnet.style.strokeDashoffset =  perimeter * (1 - target_percentage)
+}
 
+function set_pie_sector_rotation(pie_segment, current_pie_input_index, list_of_percentages) {
+    if (current_pie_input_index == 0) {
+        let degree = "0"
+        pie_segment.style.transform = "rotateZ(" + degree + "deg)"
+        return
+    }
+    if (current_pie_input_index == 1) {
+        let degree = list_of_percentages[0].toString()
+        pie_segment.style.transform = "rotateZ(" + degree + "deg)"
+        return
+    }
+    if (current_pie_input_index == 2) {
+        let degree = (list_of_percentages[0] + list_of_percentages[1]).toString()
+        pie_segment.style.transform = "rotateZ(" + degree + "deg)"
+        return
+    }
+    if (current_pie_input_index == 3) {
+        let degree = (list_of_percentages[0] + list_of_percentages[1] + list_of_percentages[2]).toString()
+        pie_segment.style.transform = "rotateZ(" + degree + "deg)"
+        return
+    }
+}
